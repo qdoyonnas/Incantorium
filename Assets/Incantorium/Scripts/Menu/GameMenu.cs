34aa7b9a1f4a6595e4c3b03f62c0e8b72d599d86
Andrew <vurtua7@hotmail.com>
2018-09-07 15:06:41 -0400

    MenuControls on controller implementation

--- Assets/Incantorium/Scripts/Menu/GameMenu.cs
+++ Assets/Incantorium/Scripts/Menu/GameMenu.cs
@@ -1,29 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using VRTK;
 
 public class GameMenu : MonoBehaviour
 {
-    public GameObject menuObject;
+    public string sceneName = "Menu";
+    public string VRTKSceneName = "VRTK";
 
+    List<GameObject> stashedRoots = new List<GameObject>();
+    Vector3 storedPosition;
+    Quaternion storedRotation;
     VRTK_ControllerEvents.ButtonAlias menuButton = VRTK_ControllerEvents.ButtonAlias.StartMenuPress;
     VRTK_ControllerEvents controllerEvents;
 
     void SubscribeMenuButton()
     {
         if( controllerEvents == null ) { return; }
-
+        
         controllerEvents.SubscribeToButtonAliasEvent( menuButton, true, ToggleMenu );
     }
 
     void ToggleMenu( object sender, ControllerInteractionEventArgs e )
     {
-        if( menuObject.activeSelf ) {
-            menuObject.SetActive( false );
+        // Find current scene state
+        Scene scene = SceneManager.GetSceneByName( sceneName );
+        if( !scene.IsValid() ) {
+            LoadMenu();
         } else {
-            menuObject.SetActive( true );
+            UnloadMenu();
         }
+    }
+
+    void LoadMenu()
+    {
+        Transform playArea = VRTK_DeviceFinder.PlayAreaTransform();
+        storedPosition = playArea.position;
+        storedRotation = playArea.rotation;
+
+        for( int i = 0; i < SceneManager.sceneCount; i++ ) {
+            Scene scene = SceneManager.GetSceneAt( i );
+            if( scene.name != VRTKSceneName ) {
+                GameObject[] roots = scene.GetRootGameObjects();
+                foreach( GameObject obj in roots ) {
+                    obj.SetActive( false );
+                    stashedRoots.Add( obj );
+                }
+            }
+        }
+
+        AsyncOperation async = SceneManager.LoadSceneAsync( sceneName, LoadSceneMode.Additive );
+    }
+    void UnloadMenu()
+    {
+        if( stashedRoots.Count == 0 || storedPosition == null || storedRotation == null ) { return; }
+
+        SceneManager.UnloadSceneAsync( sceneName );
+
+        Transform playArea = VRTK_DeviceFinder.PlayAreaTransform();
+        playArea.position = storedPosition;
+        playArea.rotation = storedRotation;
+
+        foreach( GameObject obj in stashedRoots ) {
+            obj.SetActive( true );
+        }
+        stashedRoots.Clear();
     }
 
     private void Start()
---
+++ Assets/Incantorium/Scripts/Utility/SpawnPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SpawnPoint : MonoBehaviour
+{
+    private void Start()
+    {
+        VRTK_HeadsetFade fade = VRTKScripts.instance.GetComponentInChildren<VRTK_HeadsetFade>();
+        if( fade != null ) {
+            fade.Fade( Color.black, 0 );
+        }
+
+        Transform playArea = VRTK_DeviceFinder.PlayAreaTransform();
+
+        playArea.position = this.transform.position;
+        playArea.rotation = this.transform.rotation;
+
+        OnStart();
+
+        if( fade != null ) {
+            fade.Unfade( 0.5f );
+        }
+    }
+
+    protected virtual void OnStart() {}
+}
